8c65c0bcbc95fae5312071d94f5923886a4fd8e7
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-05-06 16:14:36 +0600

    Load All Item and Single Item from Database Successfully

--- index.js
+++ index.js
@@ -9,10 +9,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Api:
-app.use('/', (req, res) => {
-        res.send("Hell from Chaldal Warehouse");
-})
+// MongoDB Connection:
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ph-5.b3v7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+        try {
+                await client.connect();
+                const itemCollections = client.db("Chaldal-Warehouse").collection("items");
+                console.log("Database Connecter")
+
+                // Root Api:
+                // app.use('/', (req, res) => {
+                //         res.send("Hello from Chaldal Warehouse");
+                // })
+
+                // Load All Items:
+                app.get("/items", async (req, res) => {
+                        const query = {};
+                        const cursor = itemCollections.find(query);
+                        const items = await cursor.toArray();
+                        res.send(items);
+                })
+
+                //Load Item by single Id:
+                app.get('/item/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: ObjectId(id) };
+                        const item = await itemCollections.findOne(query);
+                        res.send(item);
+                })
+
+        }
+        finally {
+
+        }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
         console.log("Listening from chaldal Warehouse");
